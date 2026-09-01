fd9730d1ddbe1dc94b94b073d2b724176c533d64
Anton Shabouta <zloyusr@gmail.com>
2026-09-01 18:39:37 +0300

    Make package test suites self-contained

---
+++ tests/TestCase.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Tests;
+
+use Orchestra\Testbench\TestCase as Orchestra;
+use PHPinnacle\Tempo\TempoServiceProvider;
+
+abstract class TestCase extends Orchestra
+{
+    protected function defineEnvironment($app): void
+    {
+        $app['config']->set('app.key', 'base64:AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=');
+        $app['config']->set('database.default', 'sqlite');
+        $app['config']->set('database.connections.sqlite.database', ':memory:');
+    }
+
+    protected function getPackageProviders($app): array
+    {
+        $packages = json_decode(
+            file_get_contents(__DIR__ . '/../vendor/composer/installed.json'),
+            true,
+            flags: JSON_THROW_ON_ERROR,
+        )['packages'];
+
+        $providers = array_merge(...array_map(
+            static fn (array $package): array => $package['extra']['laravel']['providers'] ?? [],
+            $packages,
+        ));
+
+        return array_values(array_unique([...$providers, TempoServiceProvider::class]));
+    }
+}
